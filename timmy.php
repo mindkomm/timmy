cb73bf9eab98685f04633d899d0e8d5bc78be523
Lukas Gaechter <lukas.gaechter@mind.ch>
2018-01-24 14:38:45 +0100

    Update plugin version

--- timmy.php
+++ timmy.php
@@ -3,7 +3,7 @@
  * Plugin Name: Timmy
  * Plugin URI: https://github.com/MINDKomm/timmy/
  * Description: Advanced image manipulation for Timber.
- * Version: 0.12.2
+ * Version: 0.13.0
  * Author: Lukas Gächter
  * Author URI: https://www.mind.ch
  */
