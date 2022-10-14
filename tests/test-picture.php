863cf8b90e0f8798a4a9bf1010669c7fcf9b3480
Lukas Gaechter <lukas.gaechter@mind.ch>
2022-10-14 17:50:35 +0200

    Use sprintf for some tests

--- tests/test-picture.php
+++ tests/test-picture.php
@@ -11,8 +11,11 @@ class TestPicture extends TimmyUnitTestCase {
 		] );
 		$result     = get_timber_picture_responsive( $attachment, 'picture' );
 
-		$expected =  '<source srcset="' . $this->get_upload_url() . '/test-560x0-c-default.jpg 560w, ' . $this->get_upload_url() . '/test-1400x0-c-default.jpg 1400w" sizes="100vw">' . PHP_EOL .
-		'<img src="' . $this->get_upload_url() . '/test-1400x0-c-default.jpg" width="1400" height="933" alt="Burrito Wrap" loading="lazy">';
+		$expected = sprintf(
+			'<source srcset="%1$s/test-560x0-c-default.jpg 560w, %1$s/test-1400x0-c-default.jpg 1400w" sizes="100vw">%2$s<img src="%1$s/test-1400x0-c-default.jpg" width="1400" height="933" alt="Burrito Wrap" loading="lazy">',
+			$this->get_upload_url(),
+			PHP_EOL
+		);
 
 		$this->assertEquals( $expected, $result );
 	}
@@ -21,8 +24,11 @@ class TestPicture extends TimmyUnitTestCase {
 		$attachment = $this->create_image();
 		$result     = get_timber_picture_responsive( $attachment, 'picture', [ 'loading' => false ] );
 
-		$expected =  '<source srcset="' . $this->get_upload_url() . '/test-560x0-c-default.jpg 560w, ' . $this->get_upload_url() . '/test-1400x0-c-default.jpg 1400w" sizes="100vw">' . PHP_EOL .
-		'<img src="' . $this->get_upload_url() . '/test-1400x0-c-default.jpg" width="1400" height="933" alt="">';
+		$expected = sprintf(
+			'<source srcset="%1$s/test-560x0-c-default.jpg 560w, %1$s/test-1400x0-c-default.jpg 1400w" sizes="100vw">%2$s<img src="%1$s/test-1400x0-c-default.jpg" width="1400" height="933" alt="">',
+			$this->get_upload_url(),
+			PHP_EOL
+		);
 
 		$this->assertEquals( $expected, $result );
 	}
@@ -33,8 +39,15 @@ class TestPicture extends TimmyUnitTestCase {
 		$image  = Timmy::get_image( $attachment->ID, 'picture' );
 		$result = $image->picture_responsive( [ 'loading' => false ] );
 
-		$expected =  '<source srcset="' . $this->get_upload_url() . '/test-560x0-c-default.jpg 560w, ' . $this->get_upload_url() . '/test-1400x0-c-default.jpg 1400w" sizes="100vw">' . PHP_EOL .
-		'<img src="' . $this->get_upload_url() . '/test-1400x0-c-default.jpg" width="1400" height="933" alt="">';
+		$expected = sprintf(
+			'<source srcset="%1$s/test-560x0-c-default.jpg 560w, %1$s/test-1400x0-c-default.jpg 1400w" sizes="100vw">%2$s<img src="%1$s/test-1400x0-c-default.jpg" width="1400" height="933" alt="">',
+			$this->get_upload_url(),
+			PHP_EOL
+		);
+
+		$this->assertEquals( $expected, $result );
+	}
+
 
 		$this->assertEquals( $expected, $result );
 	}
